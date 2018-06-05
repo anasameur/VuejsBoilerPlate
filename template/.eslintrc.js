64ea85a2294db2a70cf5a411518489fd4568ec4c
frooton <anasameur0@gmail.com>
2018-06-05 15:58:29 +0200

    package.json && eslintrc.js modification for vue init config cli

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -8,13 +8,30 @@ module.exports = {
   env: {
     browser: true,
   },
+  {{#if_eq lintConfig "standard"}}
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  {{/if_eq}}
+  {{#if_eq lintConfig "airbnb"}}
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
   extends: ['plugin:vue/essential', 'airbnb-base'],
+  {{/if_eq}}
+  {{#if_eq lintConfig "none"}}
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential'],
+  {{/if_eq}}
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
+  {{#if_eq lintConfig "airbnb"}}
   // check if imports actually resolve
   settings: {
     'import/resolver': {
@@ -23,8 +40,14 @@ module.exports = {
       }
     }
   },
+  {{/if_eq}}
   // add your custom rules here
   rules: {
+    {{#if_eq lintConfig "standard"}}
+    // allow async-await
+    'generator-star-spacing': 'off',
+    {{/if_eq}}
+    {{#if_eq lintConfig "airbnb"}}
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
@@ -44,6 +67,7 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', {
       optionalDependencies: ['test/unit/index.js']
     }],
+    {{/if_eq}}
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
