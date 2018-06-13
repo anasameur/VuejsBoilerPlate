59eda4e6eb25d0d8e5b76e26b6cccd22aa12c55b
FrOoToN <anasameur0@gmail.com>
2018-06-13 16:56:52 +0200

    Update dev.env.js

--- template/config/dev.env.js
+++ template/config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env');
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL_EXT: '"https://{host}:{port}"',
-  BASE_URL_INT: '"https://{host}:{port}"',
+  BASE_URL_INT: '"https://es2-rec1.cofely.com:443"',
   OKTA_ISSUER: '"{OKTA-Issuer}"',
   OKTA_CLIENT_ID: '"{OKTA-Client-Id}"',
   OKTA_REDIRECT_URL: '"{Application-Redirect-Url}"',
